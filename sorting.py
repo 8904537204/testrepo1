bb1f667c95150076dc4a51868ada9a66ac7216f7
Rohinimolkere <mrohini@stratapps.com>
2022-05-31 11:58:36 +0530

    adding progsorting.py

--- sorting.py
+++ sorting.py
@@ -1,6 +1,6 @@
-num=[10,45,20,50,15,75,60]
+num=[10,60,25,30,15,90,85]
 num.sort()
-print("list in ascending order:",num)
+print("list in ascend:",num)
 
 num.sort(reverse=True)
-print("list in descending order:",num)
+print("list in descen:",num)
